4b29abb49493c0ec5426dfb5682378c7bce91542
Andrew Grangaard <granny-github@ofb.net>
2011-03-30 17:28:32 -0700

    find cookbook relative to HOME

--- chef/knife.rb
+++ chef/knife.rb
@@ -1,10 +1,10 @@
 log_level                :info
 log_location             STDOUT
 node_name                'andrew'
-client_key               '/Users/andrew/.chef/andrew.pem'
+client_key               '#{ENV['HOME']}/.chef/andrew.pem'
 validation_client_name   'chef-validator'
 validation_key           '/etc/chef/validation.pem'
 chef_server_url          'http://chef.open42.com'
 cache_type               'BasicFile'
 cache_options( :path => '/Users/andrew/.chef/checksums' )
-cookbook_path [ '/home/andrew/src/chef-repo/cookbooks', '/home/andrew/src/chef-repo/site-cookbooks' ]
+cookbook_path [ "#{ENV['HOME']}/src/chef-repo/cookbooks", "#{ENV['HOME']}/src/chef-repo/site-cookbooks" ]
