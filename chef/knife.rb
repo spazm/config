562431f68ed28d359034ee9c275fabb9c2773d7e
Andrew Grangaard <granny-github@ofb.net>
2011-04-06 20:58:19 -0700

    enironment variable expansion in doublequoted strings

--- chef/knife.rb
+++ chef/knife.rb
@@ -1,10 +1,11 @@
 log_level                :info
+#log_level                :debug
 log_location             STDOUT
 node_name                'andrew'
-client_key               '#{ENV['HOME']}/.chef/andrew.pem'
+client_key               "#{ENV['HOME']}/.chef/andrew.pem"
 validation_client_name   'chef-validator'
 validation_key           '/etc/chef/validation.pem'
-chef_server_url          'http://chef.open42.com'
+chef_server_url          'http://chef.open42.com:4000'
 cache_type               'BasicFile'
-cache_options( :path => '/Users/andrew/.chef/checksums' )
+cache_options( :path => "#{ENV['HOME']}/Users/andrew/.chef/checksums" )
 cookbook_path [ "#{ENV['HOME']}/src/chef-repo/cookbooks", "#{ENV['HOME']}/src/chef-repo/site-cookbooks" ]
