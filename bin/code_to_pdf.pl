161abb3d0f13c18a2a93a3071fc84e034284368e
Andrew Grangaard <Andrew.Grangaard@demandmedia.com>
2012-09-27 16:00:56 -0700

    switches back to ps2pdf, adds python coloring

--- bin/code_to_pdf.pl
+++ bin/code_to_pdf.pl
@@ -44,7 +44,7 @@ my $enscript = 'enscript';
 my @default_args = qw( -MLetter -Gr2);
 if ( defined $line_numbers )
 {
-    my $flag  
+    my $flag
         = $line_numbers
         ? '-C' . $line_numbers
         : '-C'
@@ -52,7 +52,7 @@ if ( defined $line_numbers )
     push @default_args, ( $flag );
 }
 
-my $ps2pdf = 'pstopdf';
+my $ps2pdf = 'ps2pdf';
 
 foreach my $file (@ARGV)
 {
@@ -64,11 +64,10 @@ foreach my $file (@ARGV)
     print "$input_file -> $output_file\n";
     push @args, qw( --color -Eperl ) if $input_file =~ m/\.p[lm]$/i;
     push @args, qw( --color -Eperl ) if $input_file =~ m/\.t$/i;
-    #push @args, qw( --color -Ec )  if $input_file =~ m/\.php$/i;
-    push @args, qw( --color -Ephp )  if $input_file =~ m/\.php$/i;
+    push @args, qw( --color -Epython ) if $input_file =~ m/\.py$/i;
     push @args, ( "--title",  $file );
     my $command
-        = "$enscript @args '$input_file' -p - | $ps2pdf -i -o '$output_file'";
+        = "$enscript @args '$input_file' -p - | $ps2pdf - '$output_file'";
     print "$command\n" if $verbose;
     system $command unless $dry_run;
 }
